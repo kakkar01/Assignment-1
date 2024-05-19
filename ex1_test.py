f3f44697425d925ec0585b174ad8529eaa8c0721
Himanshu Kakkar <61901683+kakkar01@users.noreply.github.com>
2024-05-19 16:02:22 -0400

    Update ex1_test.py

--- ex1_test.py
+++ ex1_test.py
@@ -2,4 +2,8 @@ import write_your_name as name_point
 
 def test_hi_my_name_is():
     assert len(name_point.hi_my_name_is())  > 1 
+def test_name():
+    assert name_point.hi_my_name_is()=="Himanshu"
+def test_empty():
+    assert name_pont.hi_my_name_is()!=""
 
